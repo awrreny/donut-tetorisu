16df0da3cfc6839cce9cba60fb3c78e0c9a3e38d
awrreny <awrreny@gmail.com>
2024-07-31 21:49:41 +0100

    remove import* from settings.py

--- settings.py
+++ settings.py
@@ -1,31 +1,31 @@
-from pygame.locals import *
+import pygame.locals as pgl
 import random  # temporary
-from perfect_clear_info import *
+import perfect_clear_info as pci
 
 # change colours in display_info.py (scroll until it says # color schemes)
 
 # can get key codes from https://docs.w3cub.com/pygame/ref/key.html or similar website
-key_soft_drop = K_DOWN
-key_move_left = K_LEFT
-key_move_right = K_RIGHT
-key_hard_drop = K_SPACE
-key_90_clockwise = K_d
-key_90_anticlockwise = K_a
-key_180 = K_s
-key_hold = K_BACKSLASH
-key_hold2 = K_LSHIFT
-key_printboard = K_b
-key_changeCustomBag = K_n  # to input your own queue
-key_changePreset = K_m  # to input a board based on presets.py
-key_resetboard = K_F4  # window loses focus when you do this (but only once?)
-key_retryqueue = K_4  # does the same as resetboard but with the same queue (useful for retrying pc solutions)
+key_soft_drop = pgl.K_DOWN
+key_move_left = pgl.K_LEFT
+key_move_right = pgl.K_RIGHT
+key_hard_drop = pgl.K_SPACE
+key_90_clockwise = pgl.K_d
+key_90_anticlockwise = pgl.K_a
+key_180 = pgl.K_s
+key_hold = pgl.K_BACKSLASH
+key_hold2 = pgl.K_LSHIFT
+key_printboard = pgl.K_b
+key_changeCustomBag = pgl.K_n  # to input your own queue
+key_changePreset = pgl.K_m  # to input a board based on presets.py
+key_resetboard = pgl.K_F4  # window loses focus when you do this (but only once?)
+key_retryqueue = pgl.K_4  # does the same as resetboard but with the same queue (useful for retrying pc solutions)
 
 # donut controls changed in donut_transformations.py
 
 
 setFPS = 60
 
-tekoFontVisible = True  # change this to False if there is lots of text overlapping
+tekoFontVisible = False  # change this to False if there is lots of text overlapping
 
 softDropInterval = -1  # default = 50 - set to -1 for instant, hard-drop-like soft drop
 defaultGravityInterval = 10000  # default = 670 - milliseconds between gravity movement,
@@ -69,9 +69,9 @@ def create_very_custom_queue():
     #    seven_bag = ['i', 'o', 't', 's', 'z', 'j', 'l']
     #    random.shuffle(seven_bag)
     #    extra_pieces = random.randint(1, 6)
-    #    if extra_pieces_to_pc_num(extra_pieces - 1, 5) % 7 <= 3:
+    #    if pci.extra_pieces_to_pc_num(extra_pieces - 1, 5) % 7 <= 3:
     #        continue
-    #    print(f"extra_pieces: {extra_pieces - 1}  nth pc: {extra_pieces_to_pc_num(extra_pieces - 1, 5)}")
+    #    print(f"extra_pieces: {extra_pieces - 1}  nth pc: {pci.extra_pieces_to_pc_num(extra_pieces - 1, 5)}")
     #    for i in range(7-extra_pieces):
     #        seven_bag.pop()
     #    return seven_bag
